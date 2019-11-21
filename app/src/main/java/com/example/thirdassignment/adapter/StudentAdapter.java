f1298cc126854df0cb1f0c188558391573478760
Suman Bhujel <xnamus@outlook.com>
2019-11-22 00:54:13 +0545

    Adding StudentAdapter

---
+++ app/src/main/java/com/example/thirdassignment/adapter/StudentAdapter.java
@@ -0,0 +1,89 @@
+package com.example.thirdassignment.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.thirdassignment.R;
+import com.example.thirdassignment.model.Student;
+
+import java.util.List;
+
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.MyHolder> {
+
+    List<Student> studentList;
+
+    public StudentAdapter(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.student_details, parent, false);
+        MyHolder myHolder = new MyHolder(view);
+
+        return myHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, final int position) {
+        final Student student = studentList.get(position);
+        String gender = student.getGender();
+
+        holder.tName.setText(student.getFullname());
+        holder.tAge.setText(student.getAge());
+        holder.tAddress.setText(student.getAddress());
+
+        holder.tGender.setText(student.getGender());
+
+        if (gender.equals("Male")) {
+            holder.imageView.setImageResource(R.drawable.image);
+        } else if (gender.equals("Female")) {
+            holder.imageView.setImageResource(R.drawable.image);
+        } else {
+            holder.imageView.setImageResource(R.drawable.image);
+        }
+
+        holder.btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                studentList.remove(student);
+                notifyItemRemoved(position);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentList.size();
+    }
+
+    public class MyHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView tName, tAge, tAddress, tGender;
+        Button btn;
+
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imgView);
+            tName = itemView.findViewById(R.id.tvName);
+            tAge = itemView.findViewById(R.id.tvAge);
+            tAddress = itemView.findViewById(R.id.tvAddress);
+            tGender = itemView.findViewById(R.id.tvGender);
+            btn = itemView.findViewById(R.id.btnDelete);
+        }
+    }
+
+}
--- app/src/main/java/com/example/thirdassignment/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/thirdassignment/ui/dashboard/DashboardFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -17,17 +20,33 @@ import com.example.thirdassignment.R;
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    EditText etName, etAge, etAddress;
+    RadioGroup rgGender;
+    RadioButton rMale, rFemale, rOther;
+    String name, age, address;
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
+
+        etName = root.findViewById(R.id.eName);
+        etAge = root.findViewById(R.id.eAge);
+        etAddress = root.findViewById(R.id.eAddress);
+        rgGender = root.findViewById(R.id.radioGroup);
+        rMale = root.findViewById(R.id.rbMale);
+        rFemale = root.findViewById(R.id.rbFemale);
+        rOther = root.findViewById(R.id.rbOther);
+
+
+
         dashboardViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
             }
         });
         return root;
